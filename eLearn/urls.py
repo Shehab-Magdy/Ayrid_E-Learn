7fe5969d71996415faf986d441d050a482f51c15
Shehab Magdy El-Adl <cegres1@hotmail.com>
2020-01-05 21:51:20 +0200

    add subscribers to home page

--- eLearn/urls.py
+++ eLearn/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 from django.conf.urls import url
 from django.conf.urls.static import static
-from .views import registration, home, admin, rotator, members, team
+from .views import registration, home, admin, rotator, members, team, subscribers
 
 app_name = 'eLearn'
 
 urlpatterns = [
     path('', home.index, name='elearn.home'),
-    path('/register', registration.newRegistration, name='elearn.newRegistration'),
-    path('/login', registration.login, name='elearn.login'),
-    path('/admin', admin.home, name='elearn.admin'),
-    path('/admin/saverotator', rotator.saveRotator, name='elearn.saverotator'),
-    path('/admin/members', members.editMembers, name='elearn.members'),
-    path('/admin/ourTeam', team.editTeamMember, name='elearn.ourTeam'),
+    path('register/', registration.newRegistration, name='elearn.newRegistration'),
+    path('login/', registration.login, name='elearn.login'),
+    path('admin/', admin.home, name='elearn.admin'),
+    path('admin/saverotator/', rotator.saveRotator, name='elearn.saverotator'),
+    path('admin/members/', members.editMembers, name='elearn.members'),
+    path('admin/ourTeam/', team.editTeamMember, name='elearn.ourTeam'),
+    path('admin/subscribers/', subscribers.editSubscribers, name='elearn.subscribers'),
 ]
