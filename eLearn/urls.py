8d0afc3e88252d23c2d32fa24d52f74af2ceb509
Shehab Magdy El-Adl <cegres1@hotmail.com>
2020-01-07 15:04:41 +0200

    contact us with send email

--- eLearn/urls.py
+++ eLearn/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf.urls import url
 from django.conf.urls.static import static
-from .views import registration, home, admin, rotator, members, team, subscribers
+from .views import registration, home, admin, rotator, members, team, subscribers, contact_us
 
 app_name = 'eLearn'
 
@@ -9,9 +9,10 @@ urlpatterns = [
     path('', home.index, name='elearn.home'),
     path('register/', registration.newRegistration, name='elearn.newRegistration'),
     path('login/', registration.login, name='elearn.login'),
-    path('admin/', admin.home, name='elearn.admin'),
+    path('adminhome/', admin.home, name='elearn.admin'),
     path('admin/saverotator/', rotator.saveRotator, name='elearn.saverotator'),
     path('admin/members/', members.editMembers, name='elearn.members'),
     path('admin/ourTeam/', team.editTeamMember, name='elearn.ourTeam'),
     path('admin/subscribers/', subscribers.editSubscribers, name='elearn.subscribers'),
+    path('contact-us/', contact_us.contactUs, name='elearn.contactUs'),
 ]
